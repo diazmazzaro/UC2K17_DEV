5ef9559037a81770bc82cba69c7aaca8160c8ccf
Pablo Diaz Mazzaro <pdiaz@aeroterra.com>
2017-05-07 09:22:55 -0300

    new arduino

--- demos/03_widget/CustomWidget/widget.js
+++ demos/03_widget/CustomWidget/widget.js
@@ -3,6 +3,6 @@ function(declare, BaseWidget) {
   //To create a widget, you need to derive from BaseWidget.
   return declare([BaseWidget], {
     // DemoWidget code goes here 
-
+    baseClass: 'jimu-widget-demo'
   });
 });
---
+++ demos/plenaria/plenaria.bat
@@ -0,0 +1,3 @@
+start node ../13_wind/index.js
+start node ../10_custom_uiux/index.js
+start chrome -incognito "http://localhost:5013/index.html" -incognito "http://localhost:5010/index.html"
